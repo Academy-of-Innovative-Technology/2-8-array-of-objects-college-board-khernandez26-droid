d050910e6602c9fded28fe1e669fccec8bd25472
Keilah <Khernandez26@aoiths.org>
2026-01-16 14:04:49 -0500

    Completed!

---
+++ src/js/script.js
@@ -0,0 +1,67 @@
+vacationSelection1 = {
+    Name: "Switzerland",
+    Address: "Country In Europe",
+    Website: "www.switzerland.com",
+   googleMaps: "View on Google Maps",
+    fullName: function() {
+        return this.Name + " " + this.Address + " " + this.Website + " " + this.googleMaps;
+    }
+};
+
+console.log(vacationSelection.fullName());
+
+
+
+vacationSelection2 = {
+    Name: "Italy",
+    Address: "Country In Europe",
+    Website: "www.italy.com",
+   googleMaps: "View on Google Maps",
+    fullName: function() {
+        return this.Name + " " + this.Address + " " + this.Website + " " + this.googleMaps;
+    }
+};
+
+console.log(vacationSelection2.fullName()); 
+
+
+
+vacationSelection3 = {
+    Name: "Scotland",
+    Address: "Country In Europe",
+    Website: "www.scotland.com",
+   googleMaps: "View on Google Maps",
+    fullName: function() {
+        return this.Name + " " + this.Address + " " + this.Website + " " + this.googleMaps;
+    }
+};
+
+console.log(vacationSelection3.fullName());
+
+
+
+vacationSelection4 = {
+    Name: "Australia",
+    Address: "Its Own Continent",
+    Website: "www.australia.com",
+   googleMaps: "View on Google Maps",
+    fullName: function() {
+        return this.Name + " " + this.Address + " " + this.Website + " " + this.googleMaps;
+    }
+};
+
+console.log(vacationSelection4.fullName()); 
+
+
+
+vacationSelection5 = {
+    Name: "Spain",
+    Address: "Country In Europe",
+    Website: "www.spain.com",
+   googleMaps: "View on Google Maps",
+    fullName: function() {
+        return this.Name + " " + this.Address + " " + this.Website + " " + this.googleMaps;
+    }
+};
+
+console.log(vacationSelection5.fullName());
